934c0878e11a35f4704943f0d6759b239883fe5e
senaphim <senaphim@gmail.com>
2025-07-03 20:50:31 +0100

    created posts table and queries for same table. Generated code with sqlc

---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,16 @@
+-- +goose up
+CREATE TABLE posts(
+  id UUID NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  title TEXT,
+  url TEXT NOT NULL UNIQUE,
+  description TEXT,
+  published_at TIMESTAMP,
+  feed_id UUID NOT NULL REFERENCES feeds ON DELETE CASCADE,
+  CONSTRAINT fk_feeds FOREIGN KEY (feed_id) REFERENCES feeds(id)
+);
+
+-- +goose down
+DROP TABLE posts;
+
